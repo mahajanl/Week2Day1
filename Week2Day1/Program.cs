41ca2e309246ec8464b403eaaad0986ab1f6c449
mahajanl <lakshamima@gmail.com>
2017-10-02 10:52:47 -0400

    an int array, a string array, and an output

--- Week2Day1/Program.cs
+++ Week2Day1/Program.cs
@@ -10,6 +10,17 @@ namespace Week2Day1
     {
         static void Main(string[] args)
         {
+            //this is declaring - when i give it a value, i initialize it
+            // for the sake of example: int favNumber;
+            //this is how i declare an array - this is an empty array
+            // int [] favNums;
+
+            //                    these are the elements we're storing in the array - 6 elements, index 0-5
+            int[] favNumbers = { 1, 2, 3, 4, 5, 6, };
+
+            string[] studentNames = { "Raz", "Kevin", "Shane", "Katy" };
+
+            Console.WriteLine(studentNames[1]);
         }
     }
 }
